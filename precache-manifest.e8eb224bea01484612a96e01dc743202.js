4fad57dc7c1fa0ace10a309abd6062ecb8354541
karkarrahil <karkarrahil883@gmail.com>
2023-04-28 11:54:37 +0530

    Updates

---
+++ precache-manifest.e8eb224bea01484612a96e01dc743202.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "59a0b6b0fc28a2810b5f",
+    "url": "/news-app/static/css/main.12fe9ed2.chunk.css"
+  },
+  {
+    "revision": "59a0b6b0fc28a2810b5f",
+    "url": "/news-app/static/js/main.59a0b6b0.chunk.js"
+  },
+  {
+    "revision": "4ccff42df7664b317066",
+    "url": "/news-app/static/js/1.4ccff42d.chunk.js"
+  },
+  {
+    "revision": "8bea109d0dacbc20e2ab",
+    "url": "/news-app/static/js/2.8bea109d.chunk.js"
+  },
+  {
+    "revision": "a1ae735bc82301eba776",
+    "url": "/news-app/static/js/runtime~main.a1ae735b.js"
+  },
+  {
+    "revision": "0dceb307d8fbabfa3e36d3f4aa70ae02",
+    "url": "/news-app/static/media/rip.0dceb307.jpg"
+  },
+  {
+    "revision": "c02289a1b70eac47022fa5f655878bd7",
+    "url": "/news-app/index.html"
+  }
+];
